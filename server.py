42a8fe1443e206cca10830036ab04a425e270ef0
chengboxu <xuchengbo_poi@126.com>
2019-07-29 18:36:36 +0800

    添加了server端

--- server.py
+++ server.py
@@ -1,7 +1,16 @@
 from flask import Flask
+from flask import request
 from flask import render_template
 app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html',title='login')
+@app.route('/check',methods=['GET', 'POST'])
+def check():
+    if request.method == 'POST':
+        username = request.args.get('name', '')
+        password =  request.args.get('password', '')
+        if username == "admin" and password == "123":
+            return "ok"
+        else return "fail"
