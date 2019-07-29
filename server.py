1ae9e8a5e8f520954f9f1690820422301e5ce3e0
chengboxu <xuchengbo_poi@126.com>
2019-07-29 18:21:13 +0800

    添加了server端

--- server.py
+++ server.py
@@ -2,5 +2,6 @@ from flask import Flask
 from flask import render_template
 app = Flask(__name__)
 @app.route('/')
+@app.route('/index')
 def index():
     return render_template('index.html',title='login')
