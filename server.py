0184551132baaee1528dcddf2c11259d232e9782
chengboxu <xuchengbo_poi@126.com>
2019-08-01 14:13:30 +0800

    添加了注册接口

--- server.py
+++ server.py
@@ -38,7 +38,7 @@ def register():
         password =   request.form['password']
         file = open('data.json','w')
         try:
-            data_json = json.loads(file);
+            data_json = json.load(file);
             for user in data_json:
                 if user["username"]==username:
                     return "1001"
